3172cb74d13ca25484ef47e49d748f3dd8264adc
jocicnemanja <nemanja.jocic.it@gmail.com>
2023-07-21 19:25:58 +0200

    init

---
+++ src/main/java/com/server/productivity/ProductivityKitApplication.java
@@ -0,0 +1,13 @@
+package com.server.productivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductivityKitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductivityKitApplication.class, args);
+	}
+
+}
